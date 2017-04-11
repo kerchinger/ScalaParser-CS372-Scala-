923f3bdb6598fd059cbf4b05b83f4e66b5547efa
Kyle Erchinger <kerchinger@luc.edu>
2017-04-11 00:09:38 -0500

    pretty printer sort of works. Check it out, the Cond statement is closest to doiing what it should do but isn't completely correct

--- src/main/scala/PrettyPrinter.scala
+++ src/main/scala/PrettyPrinter.scala
@@ -1,66 +1,59 @@
 package edu.luc.cs.laufer.cs473.expressions
-
+import scala.language.postfixOps
 import scala.util.parsing.combinator.JavaTokenParsers
 
-/**
-  * Created by kyleerchinger on 4/10/17.
-  */
-object PrettyPrinter extends JavaTokenParsers{
+object PrettyPrinter extends JavaTokenParsers {
+  val EOL = scala.util.Properties.lineSeparator
+  val INDENT = ".."
 
-  override def skipWhitespace = true
-
-  sealed trait Node {
-    def prettyPrint(indent: Int): String
+  /** expr ::= term { { "+" | "-" } term }* */
+  def expr: Parser[Any] = (term ~ rep(("+" | "-") ~ term)) ^^ {
+    case a ~ b => (a /: b) {
+      case (x, "+" ~ y) => (x + "+" + y)
+      case (x, "-" ~ y) => (x + "-" + y)
+    }
   }
 
-  final case class Func(name: String, args: Seq[Node]) extends Node {
-    def prettyPrint(indent: Int): String =
-      (" " * indent) + name + (args match {
-        case Seq()                                =>  "()"
-        case Seq(node : Select)                   => s"(${node.prettyPrint(0)})"
-        case Seq(node : Literal)                  => s"(${node.prettyPrint(0)})"
-        case Seq(Func(name, Seq(node : Select)))  => s"($name(${node.prettyPrint(0)}))"
-        case Seq(Func(name, Seq(node : Literal))) => s"($name(${node.prettyPrint(0)}))"
-        case args => args
-          .map(_.prettyPrint(indent + 2))
-          .mkString("(\n", ",\n", "\n" + (" " * indent) + ")")
-      })
+  /** term ::= factor { { "*" | "/" | "%" } factor }* */
+  def term: Parser[Any] = (factor ~ rep(("*" | "/" | "%") ~ factor)) ^^ {
+    case a ~ b => (a /: b) {
+      case (x, "*" ~ y) => (x + "*" + y)
+      case (x, "/" ~ y) => ( x +" /" + y)
+      case (x, "%" ~ y) => (x + "%" + y)
+    }
   }
 
-  final case class Select(ids: Seq[String]) extends Node {
-    def prettyPrint(indent: Int): String = (" " * indent) + ids.mkString(".")
+  /** factor ::= wholeNumber | "+" factor | "-" factor | "(" expr ")" | ident */
+  def factor: Parser[Any] = (
+    wholeNumber ^^ { case s => s.toInt }
+      | "+" ~> factor ^^ { case e => e }
+      | "-" ~> factor ^^ { case e => e }
+      | "(" ~ expr ~ ")" ^^ { case _ ~ e ~ _ => e }
+      | ident ^^ { case f => f }
+    )
+
+  /** statement ::= expression ";" | assignment | conditional | loop | block */
+  def statement: Parser[Any] = (
+    expr <~ ";" ^^ { case s => (s + ";") }
+      | assignment
+      | conditional
+      | loop
+      | block
+    )
+
+  /** assignment ::= ident "=" expression ";" */
+  def assignment: Parser[Any] = ident ~ "=" ~ expr ~ ";" ^^ { case s ~ _ ~ r ~ _ => (s + "=" + r + ";")}
+
+  /** conditional ::= "if" "(" expression ")" block [ "else" block ] */
+  def conditional: Parser[Any] = "if" ~> ("(" ~> expr <~ ")") ~ block ~ opt("else" ~> block) ^^ {
+    case e ~ b1 ~ None => ("if" +"(" +e+ ")" + EOL +b1)
+    case e ~ b1 ~ Some(b2) => ("if" +"(" +e+ ")" + EOL +b1 + EOL+ "else" + EOL+b2)
   }
 
-  final case class Literal(text: String) extends Node {
-    def prettyPrint(indent: Int): String = (" " * indent) + text
-  }
+  /** loop ::= "while" "(" expression ")" block */
+  def loop: Parser[Any] = "while" ~ "(" ~> expr ~ ")" ~ block ^^ { case e ~ _ ~ b =>  "while" +"(" +e+ ")" +b }
 
-  def node: Parser[Node] =
-    func | select | literal
-
-  def func: Parser[Func] =
-    ident ~ "(" ~ repsep(node, ",") ~ ")" ^^ {
-      case name ~ _ ~ args ~ _ =>
-        Func(name, args)
-    }
-
-  def select: Parser[Select] =
-    rep1sep(ident, ".") ^^ {
-      case ids =>
-        Select(ids)
-    }
-
-  def literal: Parser[Literal] =
-    (wholeNumber | decimalNumber | stringLiteral | floatingPointNumber | "()") ^^ {
-      case term =>
-        Literal(term.toString)
-    }
-
-  def prettify(in: String) =
-    parseAll(node, in) match {
-      case Success(node, _)  => node.prettyPrint(0)
-      case NoSuccess(msg, _) => in
-    }
-
+  /** block ::= "{" statement* "}" */
+  def block: Parser[Any] = "{" ~> (statement) <~ "}" ^^ { case s => "{" + (s) + "}"}
 
 }
